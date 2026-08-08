f6cfa06cc59e61e190f0d5f0d32df1529be5ac58
osman gedik <osmangdk@gmail.com>
2026-08-08 21:05:21 +0300

    chore: Mobil uygulama harici tum veri transferi script ve SQL dosyalari ust dizindeki scripts klasorune tasindi

--- map_tables.sql
+++
@@ -1,4 +0,0 @@
--- places tablosuna enlem ve boylam sütunlarını ekle
-ALTER TABLE public.places 
-ADD COLUMN IF NOT EXISTS latitude DOUBLE PRECISION,
-ADD COLUMN IF NOT EXISTS longitude DOUBLE PRECISION;
